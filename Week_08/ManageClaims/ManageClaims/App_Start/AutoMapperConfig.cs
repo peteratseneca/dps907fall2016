712844390157c4a8d90a47f8ff5732f026c27176
peteratseneca <peter@Peter-MacBook-Pro.local>
2016-10-30 16:43:34 -0400

    Manage claims

---
+++ Week_08/ManageClaims/ManageClaims/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+
+namespace ManageClaims
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            // Add map creation statements here
+            // Mapper.CreateMap< FROM , TO >();
+
+            // Disable AutoMapper v4.2.x warnings
+#pragma warning disable CS0618
+
+            // AutoMapper create map statements
+
+            // ############################################################
+            // AppClaim
+
+            Mapper.CreateMap<Models.AppClaim, Controllers.AppClaimBase>();
+            Mapper.CreateMap<Controllers.AppClaimAdd, Models.AppClaim>();
+
+
+#pragma warning restore CS0618
+        }
+    }
+}
---
+++ Week_08/README.md
@@ -0,0 +1,9 @@
+### Week 8 code examples
+
+**ManageClaims**
+
+Enables a user account administer to manage the claims that are allowed in the app.
+
+Features:
+* Custom AppClaim class, to define the claim
+* Controller support for doing some basic tasks (fetch, add)
