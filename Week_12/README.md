ce957083129fddf3c6fa21ca3791a81bbd4ede70
Peter McIntyre <peter.mcintyre@senecacollege.ca>
2016-11-27 13:51:12 -0500

    SOAP XML web services, server and client

---
+++ Week_12/README.md
@@ -0,0 +1,17 @@
+### Week 12 code examples
+
+**SOAPServer**
+
+Implements a standard SOAP XML web service.  
+
+Features:
+* Adds on to an existing app from Week 3, "AssociationsOther"
+* Re-uses the same architecture as that app
+
+**SOAPClient**
+
+A standard web app, it adds the ability to work with the web service above.  
+
+Features:
+* Adds a "service reference" to the web service
+* Includes the generated proxy classes for the web service methods, and its entity classes
---
+++ Week_12/SOAPServer/AssociationsOther/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+
+namespace AssociationsOther
+{
+    // Attention 11 - AutoMapper configuration class, called from the Application_Start method (in the WebApiApplication class in Global.asax.cs)
+
+    public static class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            // Add map creation statements here
+            // Mapper.CreateMap< FROM , TO >();
+
+            // Disable AutoMapper v4.2.x warnings
+#pragma warning disable CS0618
+
+            Mapper.CreateMap<Models.Employee, Controllers.EmployeeBase>();
+            Mapper.CreateMap<Models.Employee, Controllers.EmployeeBase2>();
+            Mapper.CreateMap<Models.Employee, Controllers.EmployeeBase3>();
+            Mapper.CreateMap<Controllers.EmployeeAdd, Models.Employee>();
+
+            Mapper.CreateMap<Models.Address, Controllers.AddressBase>();
+            Mapper.CreateMap<Models.Address, Controllers.AddressFull>();
+            Mapper.CreateMap<Controllers.AddressAdd, Models.Address>();
+
+            Mapper.CreateMap<Models.JobDuty, Controllers.JobDutyBase>();
+            Mapper.CreateMap<Models.JobDuty, Controllers.JobDutyFull>();
+            Mapper.CreateMap<Controllers.JobDutyAdd, Models.JobDuty>();
+
+#pragma warning restore CS0618
+        }
+    }
+}
